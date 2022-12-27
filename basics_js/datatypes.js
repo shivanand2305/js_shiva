c8f9c41c5417d46d8bd3fd62979d737dfa7ef29f
shivanand2305 <64852426+shivanand2305@users.noreply.github.com>
2022-12-27 15:17:29 +0000

    datatypes done

---
+++ basics_js/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version.
+
+
+// alert(3+3) we are using nodejs not browser
+
+console.log("hello shiva");
+
+let name = "shivanand";
+let age = 18;
+let isLoggedIn = false;
+
+//DATATYPES
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// NULL = standalone  value
+// undefined
+// symbol = unique
+
+//object
+
+
+console.log(typeof "shivanand"); // to check type of datatypes.(string)
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
+
+
+
