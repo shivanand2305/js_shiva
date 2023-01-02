e0d5cdda8b448ef79d6c76c82cbb99151ab48e0f
shivanand2305 <64852426+shivanand2305@users.noreply.github.com>
2023-01-02 05:32:06 +0000

    conversion

---
+++ basics_js/conversion_operation.js
@@ -0,0 +1,20 @@
+let score = null;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) 
+ 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" =>33
+// "33abc" => Nan
+//true => 1; false => 0;
+
+// let isLoggedIn = 1;
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
